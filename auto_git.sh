7d90e8003f7e3b4cb1a6f91c855d18dec6d78bbb
魏喜明 <664651151@qq.com>
2020-04-16 13:18:31 +0800

    2020-04-16 系统自动提交

--- auto_git.sh
+++ auto_git.sh
@@ -2,13 +2,8 @@
 
 prt(){ echo -e "$(date +'%F %T') $(printf "%${2:-10}s" | sed "s/ /$1/g")"; }
 
-succ_erro(){
-  aa=$?
-  ( [[ $aa == 0 ]] && prt '成功' || prt '错误' ) &>> $log
-  [[ $aa != 0 ]] && continue
-}
+succ_erro(){ aa=$? && ( [[ $aa == 0 ]] && prt '成功' || prt '错误' ) &>> $log; }
 
-curr_date=
 
 home_dir=/d/Users/ximing.wei/Desktop/code
 log=$home_dir/auto_git.log
@@ -23,18 +18,19 @@ for dir in ${dirs//,/ }; do
 
   echo 'git add -u' &>> $log
   git add -u >&2 &>> $log
-  succ_erro
+  succ_erro && [[ $aa != 0 ]] && continue
 
   msg="$(date +%F) 系统自动提交"
   echo "git commit -m '$msg'" &>> $log
   git commit -m "$msg" >&2 &>> $log
-  succ_erro
+  succ_erro && [[ $aa != 0 ]] && continue
 
   echo 'git push' &>> $log
   git push >&2 &>> $log
-  succ_erro
+  succ_erro && [[ $aa != 0 ]] && continue
 
-  printf '\n\n'
+
+  printf '\n\n' &>> $log
 done
 
 
