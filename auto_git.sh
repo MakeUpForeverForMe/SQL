4217a897116139c1eaf8e0229a39cf14a5275451
魏喜明 <664651151@qq.com>
2020-04-16 12:59:12 +0800

    2020-04-16 系统自动提交

--- auto_git.sh
+++ auto_git.sh
@@ -1,37 +1,40 @@
 #!/bin/bash -e
 
-prt(){ echo -en "\n$(date +'%F %T') $(printf "%${2:-10}s" | sed "s/ /$1/g")\n"; }
+prt(){ echo -e "$(date +'%F %T') $(printf "%${2:-10}s" | sed "s/ /$1/g")"; }
 
-succ_erro(){ ( [[ $? == 0 ]] && prt '成功' || { prt '错误'; continue; } ) &>> $log; }
+succ_erro(){
+  aa=$?
+  ( [[ $aa == 0 ]] && prt '成功' || prt '错误' ) &>> $log
+  [[ $aa != 0 ]] && continue
+}
 
-curr_date=$(date +%F)
-dir_home=/d/Users/ximing.wei/Desktop/code
+curr_date=
 
-log=$dir_home/auto_git.log
+home_dir=/d/Users/ximing.wei/Desktop/code
+log=$home_dir/auto_git.log
 
 dirs=.,Project
 
-dt=$(date +'%F %T')
-
 prt '-' '50' &>> $log
 
-
 for dir in ${dirs//,/ }; do
-  cd $dir_home/$dir
-  # pwd >&2
+  cd $home_dir/$dir
   pwd &>> $log
 
   echo 'git add -u' &>> $log
   git add -u >&2 &>> $log
   succ_erro
 
-  echo "git commit -m '$curr_date 系统自动提交'" &>> $log
-  git commit -m "$curr_date 系统自动提交" >&2 &>> $log
+  msg="$(date +%F) 系统自动提交"
+  echo "git commit -m '$msg'" &>> $log
+  git commit -m "$msg" >&2 &>> $log
   succ_erro
 
   echo 'git push' &>> $log
   git push >&2 &>> $log
   succ_erro
+
+  printf '\n\n'
 done
 
 
