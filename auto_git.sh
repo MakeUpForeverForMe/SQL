4085ba546609d30b6fa76355a0c925ac879cb242
魏喜明 <ximing.wei@weshareholdings.com>
2020-04-22 21:06:42 +0800

    2020-04-22 系统自动提交

--- auto_git.sh
+++ auto_git.sh
@@ -5,15 +5,16 @@ prt(){ echo -e "$(date +'%F %T') $(printf "%${2:-10}s" | sed "s/ /$1/g")"; }
 succ_erro(){ aa=$? && ( [[ $aa == 0 ]] && prt '成功' || prt '错误' ) &>> $log; }
 
 
-home_dir=/d/Users/ximing.wei/Desktop/code
-log=$home_dir/auto_git.log
+dir1=/d/Users/ximing.wei/Desktop/code
+dir2=/d/Users/ximing.wei/Desktop/技术中心
 
-dirs=.,Project
 
+dirs=$dir1,$dir1/Project,$dir2/数仓表结构
+log=$dir1/auto_git.log
 prt '-' '50' &>> $log
 
 for dir in ${dirs//,/ }; do
-  cd $home_dir/$dir
+  cd $dir
   pwd &>> $log
 
   echo 'git add -u' &>> $log
